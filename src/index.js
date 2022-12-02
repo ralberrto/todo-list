a3883901766465cd98a2e0b8239db5f566f01e65
Rafael Alberto Larios <albertolarios@me.com>
2022-12-01 20:43:39 -0600

    Establish general web application looks with structure and style

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import "./style.css";
 const Project = function(name, description) {
     return {name, description};
 }
-
-const div = document.createElement("div");
-div.textContent = "Hello world!";
-document.body.appendChild(div);
