92f06858aa6d80b496caef99bd04b5ab23fdcac2
Rafael Alberto Larios <albertolarios@me.com>
2022-12-01 19:38:56 -0600

    Add Title font to project and link css file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello, world!")
+import "./style.css";
+
+const Project = function(name, description) {
+    return {name, description};
+}
+
+const div = document.createElement("div");
+div.textContent = "Hello world!";
+document.body.appendChild(div);
